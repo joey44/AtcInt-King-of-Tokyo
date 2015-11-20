d44b74084f04060e7f292a5d9f9e2d91b6a78fe3
joel <joel.eglin@students.fhnw.ch>
2015-11-20 13:27:15 +0100

    Test_Client_W

---
+++ src/AtcIntClient/Test_Client_W.java
@@ -0,0 +1,70 @@
+package AtcIntClient;
+import java.io.*;
+import java.net.Socket;
+import java.util.ArrayList;
+
+import AtcIntDatenaustausch.DatenAustausch;
+import AtcIntDatenaustausch.Spieler;
+import AtcIntDatenaustausch.Wurfel;
+
+public class Test_Client_W {
+
+	public static void main(String[] args) {
+		try (Socket clientSocket = new Socket("localhost", 44444);
+				ObjectOutputStream oos = new ObjectOutputStream(
+						clientSocket.getOutputStream());
+
+				ObjectInputStream ois = new ObjectInputStream(
+						clientSocket.getInputStream());
+
+		) {
+			
+			int clientID = (int) ois.readObject();
+			
+			DatenAustausch w0 = (DatenAustausch) ois.readObject();
+			System.out.println(w0);
+			
+			DatenAustausch w1 = (DatenAustausch) ois.readObject();
+			
+			
+			
+			System.out.println("Object from server:" + w1);
+			
+			ArrayList<Spieler> spielerListe = w1.getSpielerListe();
+			//Wurfel wurfel = w1.getWuerfel();
+			String moderation = w1.getModeration();
+			
+			
+			
+			w1.wurfeln();
+			
+			
+			
+			moderation = "Spieler " + clientID + " hat gewürfelt";
+			
+			w1.setModeration(moderation);
+			
+			Spieler spieler = spielerListe.get(clientID);
+			
+			System.out.println(spieler.getAnzahlLeben());
+			spieler.setAnzahlLeben(spieler.getAnzahlLeben() + 2 );
+			System.out.println(spieler.getAnzahlLeben());
+			
+			spielerListe.set(clientID, spieler);
+			
+			w1.setSpielerListe(spielerListe);
+			
+			oos.writeObject(w1);
+			
+			//DatenAustausch w1 = new DatenAustausch();
+			
+			for (int i = 0; i<6; i++){
+			DatenAustausch w2 = (DatenAustausch) ois.readObject();
+			System.out.println("client: " + w2);
+			}
+
+		} catch (Exception e) {
+			System.out.println(e.toString());
+		}
+	}
+}
